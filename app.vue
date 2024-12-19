12ba0736abd62296a300e1c8a13f04f8fc358f60
xulihang <xulihanghai@163.com>
2024-12-19 17:20:35 +0800

    initial implementation

--- app.vue
+++ app.vue
@@ -1,6 +1,57 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div id="app">
+    <h2>Barcode Scanner in Nuxt.js</h2>
+    <div v-if="!initialized">Initializing...</div>
+    <button v-if="initialized" @click="toggleScanning">{{ scanning ? "Stop Scanning":"Start Scanning" }}</button>
+    <div class="container">
+      <ClientOnly fallback-tag="span" fallback="Loading barcode scanner...">
+        <BarcodeScanner ref="scanner" @initialized="onInitialized" @scanned="onScanned"></BarcodeScanner>
+      </ClientOnly>
+    </div>
+    <div>
+      <div>
+        Results:
+      </div>
+      <ol>
+        <li v-for="(barcode,index) in scannedBarcodes" :key="'barcode-'+index">{{ barcode.formatString + ": " + barcode.text }}</li>
+      </ol>
+    </div>
   </div>
 </template>
+<script setup lang="ts">
+import type { BarcodeResultItem } from 'dynamsoft-barcode-reader-bundle';
+import BarcodeScanner from './components/BarcodeScanner.vue';
+const scanner = ref();
+const initialized = ref(false);
+const scanning = ref(false);
+const scannedBarcodes = ref<BarcodeResultItem[]>([])
+const onInitialized = () => {
+  initialized.value = true;
+}
+
+const onScanned = (barcodes:BarcodeResultItem[]) => {
+  if (barcodes.length>0) {
+    scannedBarcodes.value = barcodes;
+  }
+}
+
+const toggleScanning = () => {
+  scanning.value = !scanning.value;
+  if (scanner.value) {
+    if (scanning.value) {
+      scanner.value.start();
+    }else{
+      scanner.value.stop();
+    }
+  }
+}
+
+</script>
+
+<style lang="css" scoped>
+.container {
+  position: relative;
+  width: 360px;
+  height: 360px;
+}
+</style>
---
+++ components/BarcodeScanner.vue
@@ -0,0 +1,92 @@
+<script setup lang="ts">
+import { onMounted, onBeforeUnmount, ref, type Ref } from "vue";
+import { init } from "../dynamsoft.config";
+import { CameraEnhancer, CameraView } from "dynamsoft-camera-enhancer";
+import { CaptureVisionRouter } from "dynamsoft-capture-vision-router";
+import { MultiFrameResultCrossFilter } from "dynamsoft-utility";
+import type { BarcodeResultItem } from "dynamsoft-barcode-reader-bundle";
+
+const cameraViewContainer: Ref<HTMLElement | null> = ref(null);
+let cvRouter: CaptureVisionRouter;
+let cameraEnhancer: CameraEnhancer;
+let cameraView:CameraView;
+// define a 'detected' event that the Scanner component emits when barcodes are detected
+const emit = defineEmits<{
+  (e: 'scanned', results: BarcodeResultItem[]): void
+  (e: 'initialized'): void
+}>();
+// expose start/stop to control pause/resume scanning
+const start = async () => await startScanning();
+const stop = () => stopScanning();
+
+defineExpose({ start, stop })
+onMounted(async () => {
+  try {
+    await init();
+    // Create a `CameraEnhancer` instance for camera control and a `CameraView` instance for UI control.
+    cameraView = await CameraView.createInstance();
+    cameraEnhancer = await CameraEnhancer.createInstance(cameraView);
+
+    // Get default UI and append it to DOM.
+    cameraViewContainer.value!.append(cameraView.getUIElement());
+
+    // Create a `CaptureVisionRouter` instance and set `CameraEnhancer` instance as its image source.
+    cvRouter = await CaptureVisionRouter.createInstance();
+    cvRouter.setInput(cameraEnhancer);
+
+    // Define a callback for results.
+    cvRouter.addResultReceiver({
+      onDecodedBarcodesReceived: (result) => {
+        console.log(result);
+        emit("scanned",result.barcodeResultItems);
+      }
+    });
+
+    // Filter out unchecked and duplicate results.
+    const filter = new MultiFrameResultCrossFilter();
+    // Filter out unchecked barcodes.
+    filter.enableResultCrossVerification("barcode", true);
+    // Filter out duplicate barcodes within 3 seconds.
+    filter.enableResultDeduplication("barcode", true);
+    await cvRouter.addResultFilter(filter);
+    emit("initialized");
+  } catch (ex: any) {
+    let errMsg = ex.message || ex;
+    console.error(errMsg);
+  }
+});
+
+const stopScanning = () => {
+  cameraView?.setScanLaserVisible(false);
+  cvRouter?.stopCapturing();
+  cameraEnhancer.close();
+}
+
+const startScanning = async () => {
+  await cameraEnhancer.open();
+  cvRouter?.startCapturing("ReadSingleBarcode");
+  cameraView?.setScanLaserVisible(true);
+}
+
+// dispose cvRouter when it's no longer needed
+onBeforeUnmount(async () => {
+  try {
+    cvRouter?.dispose();
+    cameraEnhancer?.dispose();
+  } catch (_) { }
+});
+</script>
+
+<template>
+  <div ref="cameraViewContainer" id="cameraViewContainer"></div>
+</template>
+
+<style scoped>
+#cameraViewContainer {
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  position: absolute;
+}
+</style>
